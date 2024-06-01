0953484fe480310a76e1d1cc07ae2621251128a0
Jamie <adungg2509@gmail.com>
2024-06-01 21:14:16 +1000

    added svg

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -44,14 +44,20 @@ const Page = () => {
                 </div>
             </div>
             <div className={`text-xs text-white lg:col-span-2 p-3 space-y-3 bg-zinc-800 border border-zinc-100/50 rounded-md`}>
-                <p className={`text-2xs`}>adngdev/README.md</p>
+                <p className={`text-xs`}>adngdev/README.md</p>
                 <div className={`w-full h-[1px] bg-zinc-600`} />
                 <div className={`space-y-1`}>
                     <p className={`text-lg`}>Hello :)</p>
                     <p>I&apos;m Jamie from Vietnam and currently living in Melbourne.</p>
                 </div>
-                <div className={`space-y-2`}>
-                    <p className={`text-lg`}>What I Code</p>
+                <div className={`space-y-1`}>
+                    <p className={`text-lg`}>Technical Skills</p>
+                    <div className={`flex gap-1 items-center text-md`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#ffffff" viewBox="0 0 256 256">
+                            <path d="M58.34,101.66l-32-32a8,8,0,0,1,0-11.32l32-32A8,8,0,0,1,69.66,37.66L43.31,64,69.66,90.34a8,8,0,0,1-11.32,11.32Zm40,0a8,8,0,0,0,11.32,0l32-32a8,8,0,0,0,0-11.32l-32-32A8,8,0,0,0,98.34,37.66L124.69,64,98.34,90.34A8,8,0,0,0,98.34,101.66ZM200,40H176a8,8,0,0,0,0,16h24V200H56V136a8,8,0,0,0-16,0v64a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V56A16,16,0,0,0,200,40Z"></path>
+                        </svg>
+                        <p>Programming Languages</p>
+                    </div>
                     <DragDropContext onDragEnd={handleDragEnd}>
                         <Droppable droppableId={`droppable`}>
                             { provided =>
@@ -72,6 +78,12 @@ const Page = () => {
                             }
                         </Droppable>
                     </DragDropContext>
+                </div>
+                <div className={`flex gap-1 items-center text-md`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#ffffff" viewBox="0 0 256 256">
+                        <path d="M58.34,101.66l-32-32a8,8,0,0,1,0-11.32l32-32A8,8,0,0,1,69.66,37.66L43.31,64,69.66,90.34a8,8,0,0,1-11.32,11.32Zm40,0a8,8,0,0,0,11.32,0l32-32a8,8,0,0,0,0-11.32l-32-32A8,8,0,0,0,98.34,37.66L124.69,64,98.34,90.34A8,8,0,0,0,98.34,101.66ZM200,40H176a8,8,0,0,0,0,16h24V200H56V136a8,8,0,0,0-16,0v64a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V56A16,16,0,0,0,200,40Z"></path>
+                    </svg>
+                    <p>Programming Languages</p>
                 </div>
             </div>
         </div>
